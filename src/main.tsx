c5f0fec507a7f25e94e220b5996ae5b85d993598
Mudassirpk <mudassir.pk.mp@gmail.com>
2024-02-18 13:14:35 +0500

    separated deployment uis from main page

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Repos from "./pages/Repos.tsx";
@@ -8,15 +7,33 @@ import Login from "./pages/login.tsx";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import AuthProvider from "./providers/authprovider.tsx";
+import Frontenddeploymenet from "./pages/frontenddeploymenet.tsx";
+import Backenddeployment from "./pages/backenddeployment.tsx";
+import RootLayout from "./root.tsx";
+import App from "./App.tsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />,
-  },
-  {
-    path: "repos",
-    element: <Repos />,
+    element: <RootLayout />,
+    children: [
+      {
+        path: "",
+        element: <App />,
+      },
+      {
+        path: "repos",
+        element: <Repos />,
+      },
+      {
+        path: "deploy-react-frontend",
+        element: <Frontenddeploymenet />,
+      },
+      {
+        path: "deploy-node-backend",
+        element: <Backenddeployment />,
+      },
+    ],
   },
   {
     path: "login",
@@ -33,5 +50,5 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
         <RouterProvider router={router} />
       </AuthProvider>
     </QueryClientProvider>
-  </React.StrictMode>
+  </React.StrictMode>,
 );

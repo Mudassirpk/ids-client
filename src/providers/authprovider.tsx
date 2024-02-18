98876d11b36f9d579c8b486ef4ced1d1db8e47a5
Mudassirpk <mudassir.pk.mp@gmail.com>
2024-02-18 13:15:08 +0500

    added authorization

--- src/providers/authprovider.tsx
+++ src/providers/authprovider.tsx
@@ -16,7 +16,7 @@ const authContext = React.createContext<TAuthContext | null>(null);
 
 export function useAuth(): [
   TUser | null | undefined,
-  React.Dispatch<SetStateAction<TUser | null>> | null | undefined
+  React.Dispatch<SetStateAction<TUser | null>> | null | undefined,
 ] {
   const ctx = React.useContext(authContext);
   return [ctx?.user, ctx?.setUser];
@@ -30,31 +30,26 @@ export default function AuthProvider({
   useEffect(() => {
     async function verify_auth() {
       const token = localStorage.getItem("auth-token");
-      if (
-        (!token || token.length === 0 || !user) &&
-        window.location.pathname !== "/login"
-      ) {
-        console.log(window.location.pathname);
-        window.location.href = "/login";
+      if (!token || token.length === 0) {
+        if (window.location.pathname !== "/login") {
+          window.location.href = "/login";
+        }
       } else if (token) {
-        const response = await fetch(
-          "http://localhost:3001/auth/verify-token",
-          {
+        if (!user) {
+          const res = await fetch("http://localhost:3001/auth/verify-token", {
             method: "POST",
             headers: {
-              "Content-Type": "application/json;charset=utf-8",
               "x-auth-token": "Bearer " + token,
             },
-          }
-        );
-
-        if (response.status === 200) {
-          const j_res = await response.json();
-          localStorage.setItem("auth-token", j_res.token);
-          setUser(j_res.user);
-        } else {
-          if (window.location.pathname !== "/login") {
-            window.location.href = "/login";
+          });
+          if (res.status === 200) {
+            const j_res = await res.json();
+            setUser(j_res.user);
+            localStorage.setItem("auth-token", j_res.token);
+          } else if (res.status === 401) {
+            if (window.location.pathname !== "/login") {
+              window.location.href = "/login";
+            }
           }
         }
       }
@@ -64,9 +59,7 @@ export default function AuthProvider({
   }, [window.location.pathname]);
 
   const [user, setUser] = useState<TUser | null>(null);
-
   const values = { user, setUser };
-
   return (
     <authContext.Provider value={values}>
       {!user && window.location.pathname !== "/login" ? (

6b3789e96e1ee8f4ad20942c52ff92a6d964eb25
Mudassir Sarfraz <91904527+Mudassirpk@users.noreply.github.com>
2024-03-08 15:33:35 +0500

    sync test

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <h1
         className={"w-full text-center font-bold text-4xl my-6 text-blue-800"}
       >
-        IDS - V 1.0
+        IDS - V 1.0 {"<UPDATED>"}
       </h1>
       <Services />
       <FrontendServicesStatus />
